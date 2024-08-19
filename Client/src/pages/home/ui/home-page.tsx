b340937f0a686dc7ba414a3bd75b33cdd3f07be7
Edanriell <Edanriell@gmail.com>
2024-08-19 12:34:17 +0300

    working on home-page

--- Client/src/pages/home/ui/home-page.tsx
+++ Client/src/pages/home/ui/home-page.tsx
@@ -8,7 +8,83 @@ export const HomePage: Component = () => {
 	return (
 		<MainLayout>
 			<main class="home-page">
-				<h1>Home Page</h1>
+				<section class="coming-soon">
+					<div class="coming-soon__text-content">
+						<h1 class="coming-soon__title">
+							We are launching <strong>soon!</strong>
+						</h1>
+						<p class="coming-soon__text">Subscribe and get notified</p>
+					</div>
+					<form class="coming-soon__notify-form notify-form" method="post">
+						<div class="notify-form__field">
+							<label class="visually-hidden" for="email">
+								Email address
+							</label>
+							<input
+								class="notify-form__input"
+								type="email"
+								name="email"
+								id="email"
+								placeholder="Your email address…"
+							/>
+						</div>
+						<button class="notify-form__submit-button button" type="submit">
+							Notify Me
+						</button>
+					</form>
+					<div class="coming-soon__dashboard-image dashboard-image">
+						<picture>
+							<source
+								type="image/svg+xml"
+								media="(min-width: 1440px)"
+								srcset="/images/vector/dashboard-image-lg.svg"
+							/>
+							<source
+								type="image/svg+xml"
+								media="(min-width: 375px)"
+								srcset="/images/vector/dashboard-image-sm.svg"
+							/>
+							<img
+								class="dashboard-image__main-image"
+								src="/images/vector/dashboard-image-sm.svg"
+								alt="Ping dashboard demo image"
+							/>
+						</picture>
+						<div class="dashboard-image__slack-logotype-wrapper">
+							<picture>
+								<source
+									type="image/webp"
+									media="(min-width: 1440px)"
+									srcset="/images/raster/slack-logotype-lg@1x.webp 1x, /images/raster/slack-logotype-lg@2x.webp 2x"
+								/>
+								<source
+									type="image/webp"
+									media="(min-width: 375px)"
+									srcset="/images/raster/slack-logotype-sm@1x.webp 1x, /images/raster/slack-logotype-sm@2x.webp 2x"
+								/>
+								<source
+									type="image/jpg"
+									media="(min-width: 1440px)"
+									srcset="/images/raster/slack-logotype-lg@1x.png 1x, /images/raster/slack-logotype-lg@2x.png 2x"
+								/>
+								<source
+									type="image/jpg"
+									media="(min-width: 375px)"
+									srcset="/images/raster/slack-logotype-sm@1x.png 1x, /images/raster/slack-logotype-sm@2x.png 2x"
+								/>
+								<img
+									class="dashboard-image__slack-logotype"
+									src="/images/raster/slack-logotype-sm@1x.png"
+									srcSet="/images/raster/slack-logotype-sm@2x.png"
+									alt="Slack compay logotype"
+								/>
+							</picture>
+						</div>
+						<div class="dashboard-image__decorative-green-rectangle"></div>
+						<div class="dashboard-image__decorative-cross-rectangle dashboard-image__decorative-cross-rectangle--type--small"></div>
+						<div class="dashboard-image__decorative-cross-rectangle dashboard-image__decorative-cross-rectangle--type--large"></div>
+					</div>
+				</section>
 			</main>
 		</MainLayout>
 	);
