5be5ed0dc2280556fe9c4aaed618c5e0e269acb6
Edanriell <Edanriell@gmail.com>
2024-08-21 11:18:08 +0300

    button finished

--- Client/src/shared/ui/button/ui/button.tsx
+++ Client/src/shared/ui/button/ui/button.tsx
@@ -1,5 +1,21 @@
 import "./button.less";
 
-export const Button = () => {
-	return null;
+import { children, Component, JSX, mergeProps } from "solid-js";
+
+type ButtonProps = {
+	type?: "button" | "submit" | "reset";
+	classes?: string;
+	children: JSX.Element;
+};
+
+export const Button: Component<ButtonProps> = (props) => {
+	const finalProps = mergeProps({ type: "button", classes: "" }, props);
+
+	const safeChildren = children(() => props.children);
+
+	return (
+		<button class={finalProps.classes + " button"} type={finalProps.type as any}>
+			{safeChildren()}
+		</button>
+	);
 };
